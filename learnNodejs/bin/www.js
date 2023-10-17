e774109291da79e7f5cd5ac0447a2205f02e0575
janlin002 <123davidbill@gmail.com>
2023-10-17 22:24:46 +0800

    feat: change-commonjs-to-esm

--- learnNodejs/bin/www.js
+++ learnNodejs/bin/www.js
@@ -4,9 +4,11 @@
  * Module dependencies.
  */
 
-var app = require('../app');
-var debug = require('debug')('learnnodejs:server');
-var http = require('http');
+import app from '../app.js';
+import debug from 'debug';
+import http from 'http';
+
+const logger = debug('learnnodejs:server');
 
 /**
  * Get port from environment and store in Express.
@@ -86,5 +88,5 @@ function onListening() {
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  logger('Listening on ' + bind);
 }
