a80bc6a5f2d82148c2b8cbafe0c14d0706d9a686
janlin002 <123davidbill@gmail.com>
2023-11-22 00:23:00 +0800

    feat: add auth module

--- nestWithPostgreSql/src/app.module.ts
+++ nestWithPostgreSql/src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { TasksModule } from './tasks/tasks.module';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
