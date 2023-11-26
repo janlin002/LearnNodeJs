b553ba9ab418e9cb1c822e2b5529b2b0c4ae2a5d
janlin002 <123davidbill@gmail.com>
2023-11-26 20:02:57 +0800

    add swagger

--- nestWithPostgreSql/src/auth/dto/auth-credential.dto.ts
+++ nestWithPostgreSql/src/auth/dto/auth-credential.dto.ts
@@ -1,9 +1,15 @@
 import { IsNotEmpty, Matches, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthCredentialDto {
   @IsNotEmpty()
   @MinLength(4)
   @MaxLength(20)
+  @ApiProperty({
+    minLength: 4,
+    maxLength: 20,
+    description: '使用者名稱',
+  })
   username: string;
 
   @IsNotEmpty()
@@ -11,6 +17,11 @@ export class AuthCredentialDto {
   @MaxLength(32)
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message: 'password is to weak',
+  })
+  @ApiProperty({
+    minLength: 8,
+    maxLength: 32,
+    description: '使用者密碼',
   })
   password: string;
 }
--- nestWithPostgreSql/src/main.ts
+++ nestWithPostgreSql/src/main.ts
@@ -1,11 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { Logger } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
+  // const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
+}
+
+function setupSwagger(app: INestApplication) {
+  const builder = new DocumentBuilder();
+  const config = builder
+    .setTitle('Swagger')
+    .setDescription('my first nest swagger')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 }
 bootstrap();
