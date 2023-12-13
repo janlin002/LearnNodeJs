11889affb2043fd0e3b8186cafd58715e60dd5cf
janlin002 <123davidbill@gmail.com>
2023-12-13 12:36:57 +0800

    .

---
+++ nestAuthDraft/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'postgres',
+      username: 'postgres',
+      autoLoadEntities: true,
+      database: 'pgWithNest',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
