786f0a545a730a56b5da476f8ea15b901feae5eb
janlin002 <123davidbill@gmail.com>
2023-12-24 11:54:52 +0800

    feat: add Interceptors & filter

---
+++ nestWithLifeCycle/src/interceptor/response.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const http = context.switchToHttp();
+    const response = http.getResponse();
+    response.status(200);
+
+    return next.handle().pipe(
+      map((data) => ({
+        from: process.env.PROJECT_CODE || '',
+        error: 0,
+        message: '',
+        data: data || null,
+        token: '',
+      })),
+    );
+  }
+}
